516f7843704bee5c43e275d75b168f8990e54135
Kelvin <549652152@qq.com>
2019-10-09 16:52:03 +0800

    bubb

---
+++ practise/bubbleSort.py
@@ -0,0 +1,9 @@
+def bubbleSort(arr):
+    for i in range(1,len(arr)):
+        for j in range(0,len(arr)-1):
+            if arr[j]>arr[j+1]:
+                arr[j],arr[j+1]=arr[j+1],arr[j]
+    return arr
+arr1 = [1,5,9,6,3,4,6,0]
+bubbleSort(arr1)
+print(arr1)
---
+++ practise/dict.py
@@ -0,0 +1,5 @@
+book = dict()
+book["apple"] = 0.67
+book["mild"] = 1.49
+book["avocado"] = 1.49
+print(book)
